147c0c079b252c643c155caaee16ea455d8c65c6
SandyArdiyan <sanardiyan881@gmail.com>
2024-04-23 16:28:17 +0700

    Comit 2

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -4,4 +4,10 @@ using namespace std;
 class Mahasiswa{
 public: // akses modifier
 	string nama;
+	int umur;
+	string jurusan;
 	
+	void output(){
+		cout << "Nama: " << nama << endl;
+		cout << "umur: " << umur << endl;
+		
